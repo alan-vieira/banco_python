b03173f3e06e49ca712ce15710a1ffd6c25f6d31
Alan Vieira <66126584+alan-vieira@users.noreply.github.com>
2023-07-26 19:35:57 -0300

    Implantação do salvamento das operações em um dict

--- operacao.py
+++ operacao.py
@@ -7,29 +7,41 @@ menu = '''
 
 => '''
 
+extrato_dict = {}
+operacao_list = []
+valor_list = []
 saldo = 0
 LIMITE = 500
 extrato = ''
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+
 while True:
 
     opcao = input(menu)
 
+    # depósito
     if opcao == 'd':
         valor = float(input('Informe o valor do depósito: '))
 
         if valor > 0:
             saldo += valor
-            extrato += f'Depósito: R$ {valor:.2f}\n'
+
+            # registro da operação
+            operacao_list.append('Depósito')
+            valor_list.append(f'R$ {valor:.2f}')
+            extrato_dict['operacao'] = operacao_list
+            extrato_dict['valor'] = valor_list
 
         else:
             print('Falha de operação! O valor informado é invalido')
 
+    # saque
     elif opcao == 's':
         valor = float(input('Informe o valor do saque: '))
 
+        # regras
         excedeu_saldo = valor > saldo
         excedeu_limite = valor > LIMITE
         excedeu_saques = numero_saques >= LIMITE_SAQUES
@@ -45,21 +57,37 @@ while True:
 
         elif valor > 0:
             saldo -= valor
-            extrato += f'Saque: R$ {valor:.2f}\n'
+
+            # registro da operação
+            operacao_list.append('Saque')
+            valor_list.append(f'R$ {valor:.2f}')
+            extrato_dict['operacao'] = operacao_list
+            extrato_dict['valor'] = valor_list
+
             numero_saques += 1
 
         else:
             print('Falha de operação! O valor informado é inválido.')
 
+    # extrato
     elif opcao == 'e':
         print('\n================= EXTRATO =================')
-        print('Não foram realizadas movimentações.' if not extrato else extrato)
-        print(f'\nSaldo: R$ {saldo:.2f}')
-        print('===========================================')
+        if extrato_dict == {}:
+            print('Não foram realizadas movimentações.')
+            print(f'\nSaldo: R$ {saldo:.2f}')
+            print('===========================================')
 
+        else:
+
+            for op, val in zip(extrato_dict['operacao'], extrato_dict['valor']):
+                print(f'{op}: {val}')
+
+            print(f'\nSaldo: R$ {saldo:.2f}')  
+            print('===========================================')
+
+    # encerramento
     elif opcao == 'q':
         break
 
     else:
         print('Operação inválida, por favor selecione novamente a operação desejada.')
-
